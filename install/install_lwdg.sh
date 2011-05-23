f161fd24aec0c254e91cd8146509b3c32be99d0e
Michal Machin <ltwood@gmail.com>
2011-05-23 18:59:10 +0400

    Добавлена поддержка подзадач при развертывании окружения.
    В сообщениях, выводимых при запуске задач, более мелких,
    чем подзадачи, делается дополнительный отступ.

--- install/install_lwdg.sh
+++ install/install_lwdg.sh
@@ -1,1 +1,7 @@
-./temp/standalone-lua.exe tools/install_lwdg.lua
+if [ -n "$*" ]
+then
+  ./temp/standalone-lua.exe tools/install_lwdg.lua $*
+else
+  ./temp/standalone-lua.exe tools/install_lwdg.lua setenv testprg createtree reglua lutils extutl localutlsh
+fi
+
--- install/tools/install_lwdg.lua
+++ install/tools/install_lwdg.lua
@@ -1,183 +1,203 @@
 -- Этот скрипт не следует запускать вручную.
 
+-- В скрипте используются относительные пути в предположении,
+-- что скрипт был запущен из директории install репозитория utils
+
 dofile('tools/istools.lua')
+
+--
+-- Формируем список задач
+--
+tasks = {}
+tasks_str = ""
+for j =1, #arg do
+  tasks[arg[j]] = true
+  tasks_str = tasks_str .. arg[j] .. ' '
+end
+msg("Tasks: " .. tasks_str)
+
+home = get_cfg_path('home')
 
 --
 -- Устанавливаем переменные среды
 --
-msg "Setting environment variables..."
+if tasks['setenv'] then
+  msg "Setting environment variables..."
 
-do
-  local pe = get_env('pathext')
-  local pe_tbl = split(pe)
-  if not pe_tbl['.lua'] then
-    set_env('pathext', pe .. ';.lua')
+  do
+    local pe = get_env('pathext')
+    local pe_tbl = split(pe)
+    if not pe_tbl['.lua'] then
+      set_env('pathext', pe .. ';.lua')
+    end
   end
-end
 
-home = get_cfg_path('home')
+  set_env("lwml_zzz", ":log:dump:jit")
 
-set_env("lwml_zzz", ":log:dump:jit")
+  set_env("lua_path", string.format("./?.lua;%s/lutils/?.lua;%s/lutils/lib/?.lua", home, home))
+  set_env("lua_cpath", string.format("./?.dll;%s/share/?.dll", home))
 
-set_env("lua_path", string.format("./?.lua;%s/lutils/?.lua;%s/lutils/lib/?.lua", home, home))
-set_env("lua_cpath", string.format("./?.dll;%s/share/?.dll", home))
+  set_env("lwdg_home", home)
 
-set_env("lwdg_home", home)
+  set_env("wxwin", home .. "/wx/")
 
-set_env("wxwin", home .. "/wx/")
+  log('# current path: <' .. get_env('path') ..'>')
+  path_tbl = split(get_env('path'))
+  upath_tbl = split(get_user_path() or '')
 
-log('# current path: <' .. get_env('path') ..'>')
-path_tbl = split(get_env('path'))
-upath_tbl = split(get_user_path() or '')
+  local path_changed = false
 
-local path_changed = false
-
-function test_path( path )
-  path = norm_path(path)
-  if not path_tbl[path] then
-    path_tbl[path] = path
-    upath_tbl[path] = path
-    path_changed = true
-    log('PATH=$PATH:' .. path)
+  function test_path( path )
+    path = norm_path(path)
+    if not path_tbl[path] then
+      path_tbl[path] = path
+      upath_tbl[path] = path
+      path_changed = true
+      log('PATH=$PATH:' .. path)
+    end
   end
-end
 
-test_path(get_cfg_path('mingw') .. '/bin')
-test_path(get_cfg_path('msys') .. '/bin')
+  test_path(get_cfg_path('mingw') .. '/bin')
+  test_path(get_cfg_path('msys') .. '/bin')
 
-test_path(home .. '/utils')
-test_path(home .. '/share')
-test_path(home .. '/lutils')
+  test_path(home .. '/utils')
+  test_path(home .. '/share')
+  test_path(home .. '/lutils')
 
-test_path(home .. "/wx/build-lwdg-dll/lib")
+  test_path(home .. "/wx/build-lwdg-dll/lib")
 
-if path_changed then
-  msg "Setting PATH..."
-  set_user_env('path', join(upath_tbl))
-  set_proc_env('path', join(path_tbl))
-else
-  msg "PATH is ok."
+  if path_changed then
+    msg "  Setting PATH..."
+    set_user_env('path', join(upath_tbl))
+    set_proc_env('path', join(path_tbl))
+  else
+    msg "  PATH is ok."
+  end
 end
 
 --
 -- Проверяем наличие программ и их версии.
 --
-msg "Testing standard programs..."
+if tasks['testprg'] then
+  msg "Testing standard programs..."
 
--- проверяем наличие программ
-
-function test_exist( prog, msg )
-  if exec(prog .. ' --version >nul 2>nul') ~= 0 then
-    stop("can't run <%s>, %s", prog, msg)
-  end
-end
-
--- msys
-test_exist('sh.exe', 'please, install msys (1.0.10)')
-
--- mingw
-test_exist('strip.exe', 'please, install mingw (package binutils-2.17.50)')
-test_exist('dos2unix.exe', 'please, install mingw (package mingw-utils-0.3)')
-test_exist('gcc.exe', 'please, install mingw (package gcc-core-3.4.5)')
-test_exist('g++.exe', 'please, install mingw (package gcc-g++-3.4.5)')
-
---
--- проверяем версии программ
---
-msg "Testing versions of programs..."
-
-function test_ver( prog, ver )
-  local fn = '.' .. os.tmpname()
-  if exec(prog .. ' --version >' .. fn) ~= 0 then
-    stop("can't run <%s>", prog)
-  end
-  local file = assert(io.open(fn, 'rt'))
-  local pver = file:read('*line')
-  file:close()
-  os.remove(fn)
-
-  local pver_label = pver:match('(%d+%.%d+%.%d+)')
-  local pver_l1, pver_l2, pver_l3 = pver:match('(%d+)%.(%d+)%.(%d+)')
-  if type(ver) == 'string' and pver ~= ver then
-    return
-  elseif type(ver) == 'function' and ver(pver_l1, pver_l2, pver_l3) then
-    return
-  else
-    if type(ver) == 'string' then
-      msg('** expected: ver=<' .. ver .. '>')
-      msg('** ...found: ver=<' .. pver .. '>')
+  -- проверяем наличие программ
+  function test_exist( prog, msg )
+    if exec(prog .. ' --version >nul 2>nul') ~= 0 then
+      stop("can't run <%s>, %s", prog, msg)
     end
-    stop("incorrect version of <%s>", prog)
   end
-end
 
-test_ver('sh.exe', '2.04.0')
-test_ver('gcc.exe', '3.4.5')
+  -- msys
+  test_exist('sh.exe', 'please, install msys (1.0.10)')
+
+  -- mingw
+  test_exist('strip.exe', 'please, install mingw (package binutils-2.17.50)')
+  test_exist('dos2unix.exe', 'please, install mingw (package mingw-utils-0.3)')
+  test_exist('gcc.exe', 'please, install mingw (package gcc-core-3.4.5)')
+  test_exist('g++.exe', 'please, install mingw (package gcc-g++-3.4.5)')
+
+  -- проверяем версии программ
+  function test_ver( prog, ver )
+    local fn = '.' .. os.tmpname()
+    if exec(prog .. ' --version >' .. fn) ~= 0 then
+      stop("can't run <%s>", prog)
+    end
+    local file = assert(io.open(fn, 'rt'))
+    local pver = file:read('*line')
+    file:close()
+    os.remove(fn)
+
+    local pver_label = pver:match('(%d+%.%d+%.%d+)')
+    local pver_l1, pver_l2, pver_l3 = pver:match('(%d+)%.(%d+)%.(%d+)')
+    if type(ver) == 'string' and pver ~= ver then
+      return
+    elseif type(ver) == 'function' and ver(pver_l1, pver_l2, pver_l3) then
+      return
+    else
+      if type(ver) == 'string' then
+        msg('** expected: ver=<' .. ver .. '>')
+        msg('** ...found: ver=<' .. pver .. '>')
+      end
+      stop("incorrect version of <%s>", prog)
+    end
+  end
+
+  test_ver('sh.exe', '2.04.0')
+  test_ver('gcc.exe', '3.4.5')
+end
 
 --
 -- создаем отсутствующие поддиректории домашнего каталога
 --
-msg "Creating directory tree..."
+if tasks['createtree'] then
+  msg "Creating directory tree..."
 
-execf('sh -c "mkdir -p %s/include"', home)
-execf('sh -c "mkdir -p %s/lib"', home)
-execf('sh -c "mkdir -p %s/lutils/lib"', home)
-execf('sh -c "mkdir -p %s/share"', home)
-execf('sh -c "mkdir -p %s/utils"', home)
+  execf('sh -c "mkdir -p %s/include"', home)
+  execf('sh -c "mkdir -p %s/lib"', home)
+  execf('sh -c "mkdir -p %s/lutils/lib"', home)
+  execf('sh -c "mkdir -p %s/share"', home)
+  execf('sh -c "mkdir -p %s/utils"', home)
+end
 
 --
 -- регистрируем расширение .lua
 --
-msg "Registering lua..."
+if tasks['reglua'] then
+  msg "Registering lua..."
 
-reg_ext('.lua', 'luascript', home .. '/utils/lua.exe')
+  reg_ext('.lua', 'luascript', home .. '/utils/lua.exe')
+end
 
 --
 -- Построение lua-подсистемы
 --
-msg "Building lua utilities..."
+if tasks['lutils'] then
+  msg "Building lua utilities..."
 
--- копируем lua-утилиты
-lua_make('lutils', 'setup.lua')
+  -- копируем lua-утилиты
+  lua_make('lutils', 'setup.lua')
+end
 
 --
 -- Сборка внешних утилит
 --
+if tasks['extutl'] then
+  msg "Building external utilities..."
 
-do
   -- собираем lua
-  msg "Building lua interpreter..."
+  msg "  Building lua interpreter..."
   local lua_path = 'third-party/lua-addons/setup'
   execf('cp temp/standalone-lua.exe ../%s', lua_path)
   lua_make(lua_path, 'build_lua.lua')
   os.remove('../' .. lua_path .. '/' .. 'standalone-lua.exe')
 
   -- собираем libjpeg
-  msg "Building libjpeg..."
+  msg "  Building libjpeg..."
   lua_make('third-party/libjpeg')
 
   -- собираем libzlib
-  msg "Building libzlib..."
+  msg "  Building libzlib..."
   lua_make('third-party/zlib')
 
   -- собираем libtiff
-  msg "Building libtiff..."
+  msg "  Building libtiff..."
   lua_make('third-party/libtiff')
 
   -- собираем сторонние lua-модули
-  msg "Building lua modules..."
+  msg "  Building lua modules..."
   lua_make(lua_path, 'build_lfs.lua')
   lua_make(lua_path, 'build_md5.lua')
 
   -- компилируем специфичные lua-скрипты
-  msg "Building lua scripts..."
+  msg "  Building lua scripts..."
   local hlpath = '../lutils' --!!
   execf('lua %s/lutils/luaccc.lua %s/utils/lred.lua >nul', home, hlpath)
   execf('lua %s/lutils/luaccc.lua %s/llake/llake.lua >nul', home, hlpath)
   execf('mv lred.exe llake.exe %s/utils', home)
 
   -- собираем сторонние утилиты
-  msg "Building external utilities..."
+  msg "  Building additional utilities..."
   lua_make('third-party/ccalc')
   lua_make('third-party/dbmon')
   lua_make('third-party/winutl')
@@ -194,31 +214,33 @@ end
 --
 -- Сборка утилит и библиотек, использующих при сборке llake.
 --
-msg "Building local utilities..."
+if tasks['localutl'] then
+  msg "Building local utilities..."
 
-function make( path, name, dst )
-  cdrun(path, 'llake -s make build.llk 2>nul 1>nul')
-  local src = '../' .. path .. '/' .. name
-  local dst = home .. '/' .. dst
-  execf("mv %s %s", src, dst)
+  function make( path, name, dst )
+    cdrun(path, 'llake -s make build.llk 2>nul 1>nul')
+    local src = '../' .. path .. '/' .. name
+    local dst = home .. '/' .. dst
+    execf("mv %s %s", src, dst)
+  end
+
+  -- независимые от lwml утилиты
+
+  make('secluded/ldatav', 'ldatav.exe', 'utils')
+  make('secluded/limlib', 'limlib.dll', 'share')
+  make('secluded/llogsrv', 'llogsrv.dll', 'share')
+
+  -- зависимые от lwml утилиты
+
+  make('lwml-dep/dllver', 'dllver.exe', 'utils')
+  make('lwml-dep/limcov', 'limcov.dll', 'share')
+  make('lualib/lswg', 'lswg.dll', 'share')
+  make('lualib/lualwml', 'lualwml.dll', 'share')
+  make('lwml-dep/lwhich', 'lwhich.exe', 'utils')
+
+  -- копирование
+
+  local hlc_path = '../lwml-dep/limcov'
+  execf('cp %s/limcov_dll.h %s/include', hlc_path, home)
+  execf('cp %s/limcov.a %s/lib/liblimcov.a', hlc_path, home)
 end
-
--- независимые от lwml утилиты
-
-make('secluded/ldatav', 'ldatav.exe', 'utils')
-make('secluded/limlib', 'limlib.dll', 'share')
-make('secluded/llogsrv', 'llogsrv.dll', 'share')
-
--- зависимые от lwml утилиты
-
-make('lwml-dep/dllver', 'dllver.exe', 'utils')
-make('lwml-dep/limcov', 'limcov.dll', 'share')
-make('lualib/lswg', 'lswg.dll', 'share')
-make('lualib/lualwml', 'lualwml.dll', 'share')
-make('lwml-dep/lwhich', 'lwhich.exe', 'utils')
-
--- копирование
-
-local hlc_path = '../lwml-dep/limcov'
-execf('cp %s/limcov_dll.h %s/include', hlc_path, home)
-execf('cp %s/limcov.a %s/lib/liblimcov.a', hlc_path, home)
