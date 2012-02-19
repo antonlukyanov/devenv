0c33aacd4486f85310f1a12fb8f880500dbe50f5
Michal Machin <ltwood@gmail.com>
2012-02-19 22:32:23 +0400

    Реорганизация процесса сборки интерпретатора lua.
    Удален неиспользуемый скрипт сборки для VS.
    Патч перенесен в директорию third-party/lua-addons/setup/patch.
    Откачены изменения в файле luaconf.h
    (они делались вручную применением патча при обновлении версии lua).
    В скрипт build_lua.lua добавлены применение и откат патча.

--- third-party/lua-addons/setup/build_lua.lua
+++ third-party/lua-addons/setup/build_lua.lua
@@ -14,10 +14,12 @@ for _, fn in ipairs(lua_modules) do
   mlist = mlist .. src_path .. fn .. '.c '
 end
 
+__("patch ../../lua51/src/luaconf.h patch/lua-5_1.diff")
 __("g++ -shared -Wl,--out-implib,liblua51.a -o lua51.dll -O2 -Wall -DLUA_BUILD_AS_DLL " .. mlist .. " 2>nul")
 __("g++ -o lua.exe -DLUA_BUILD_AS_DLL " .. src_path.."lua.c liblua51.a")
 __("g++ -o luac.exe -O2 -Wall " .. src_path.."luac.c " .. src_path.."print.c " .. mlist)
 __("strip --strip-unneeded lua51.dll lua.exe luac.exe")
+__("patch -R ../../lua51/src/luaconf.h patch/lua-5_1.diff")
 
 -- setup
 
