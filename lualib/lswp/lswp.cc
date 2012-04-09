effb95c4c5a7f07dd1f29ed8342f9e71edcf1aab
Michal Machin <ltwood@gmail.com>
2012-04-10 03:19:00 +0400

    Добавлены функции sleep(), kill() и ps().

--- lualib/lswp/lswp.cc
+++ lualib/lswp/lswp.cc
@@ -1,6 +1,7 @@
 #include "luaextn.h"
 
 #include <windows.h>
+#include <tlhelp32.h>
 
 #include "revision.hg"
 
@@ -101,11 +102,120 @@ LUA_TRY
 LUA_CATCH(ex)
 }
 
+//
+
+LUA_DEF_FUNCTION( sleep, L )
+{
+  luaextn ex(L);
+LUA_TRY
+  int tm = (ex.arg_num() == 1) ? ex.get_int(1) : 0;
+  Sleep(tm);
+  return ex.ret_num();
+LUA_CATCH(ex)
+}
+
+void put_win_error( luaextn& ex, const char* pref )
+{
+  char buffer[1024];
+  FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, 0, GetLastError(), 0, buffer, sizeof(buffer), 0);
+  ex.put_error("%s: %s", pref, buffer);
+}
+
+LUA_DEF_FUNCTION( kill, L )
+{
+  luaextn ex(L);
+LUA_TRY
+
+  int pid = ex.get_int(1);
+
+  HANDLE hProc = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
+
+  if( hProc == NULL )
+    ex.put_error("lswp.kill: incorrect pid");
+  else {
+    if( !TerminateProcess(hProc,1) )
+      ex.put_error("lswp.kill: can't terminate");
+    else
+      ex.put_bool(true);
+    CloseHandle(hProc);
+  }
+
+  return ex.ret_num();
+
+LUA_CATCH(ex)
+}
+
+namespace {
+  void push_proc_tbl( lua_State* L, int pid, const char* name, const char* path )
+  {
+    lua_newtable(L);
+    
+    lua_pushstring(L, "pid");
+    lua_pushnumber(L, pid);
+    lua_settable(L, -3);
+
+    lua_pushstring(L, "name");
+    lua_pushstring(L, name);
+    lua_settable(L, -3);
+
+    lua_pushstring(L, "path");
+    lua_pushstring(L, path);
+    lua_settable(L, -3);
+  }
+};
+
+LUA_DEF_FUNCTION( ps, L )
+{
+  luaextn ex(L);
+LUA_TRY
+
+  HANDLE hSnap=CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS,0);
+
+  if( hSnap == INVALID_HANDLE_VALUE )
+    ex.put_error("lswp.ps: can't enumerate processes");
+  else {
+    PROCESSENTRY32 pe;
+    pe.dwSize = sizeof(pe);
+
+    lua_newtable(L);
+    if( Process32First(hSnap, &pe) ){
+      int idx = 1;
+      do{
+        MODULEENTRY32 me;
+        HANDLE hMod;
+        if( pe.th32ProcessID == 0 )
+          continue;
+        hMod = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pe.th32ProcessID);
+        if( hMod == INVALID_HANDLE_VALUE )
+          continue;
+        me.dwSize = sizeof(me);
+        if( Module32First(hMod,&me) ){
+          lua_pushnumber(L, idx++);
+          push_proc_tbl(L, pe.th32ProcessID, me.szModule, me.szExePath);
+          lua_settable(L, -3);
+        }
+        CloseHandle(hMod);
+      }while( Process32Next(hSnap, &pe) );
+    }
+    ex.i_have_created_stack_element();
+    CloseHandle(hSnap);
+    ex.put_bool(true);
+  }
+
+  return ex.ret_num();
+
+LUA_CATCH(ex)
+}
+
 LUA_BEGIN_LIBRARY(lswp)
   LUA_FUNCTION(gpf_off)
   LUA_FUNCTION(set_background)
   LUA_FUNCTION(run_min)
   LUA_FUNCTION(run_hide)
+
+  LUA_FUNCTION(sleep)
+  LUA_FUNCTION(kill)
+  LUA_FUNCTION(ps)
 LUA_END_LIBRARY
 
 LUA_BEGIN_EXPORT(lswp)
